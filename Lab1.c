5a2c9762fa81bd4d3ccbe5dbd53dd8084e22e040
mariyanavsegda-alt <mariyanavsegda@gmail.com>
2025-10-02 20:20:54 +0300

    Update Lab1.c

--- Lab1.c
+++ Lab1.c
@@ -12,7 +12,6 @@ double calculateZ(double x, double y) {
         return sqrt(x * x * y + 8);
     }
 }
-
 int main() {
     double hx = 0.5;
     double hy = 1.2;
@@ -20,28 +19,24 @@ int main() {
     double xk = 1.0;
     double yn = 1.0;
     double yk = 3.0;
-    
+
     double sum = 0.0;
-    int count = 0;
-  
-    printf("Значения Z > 0.2:\n");
-    printf(" x\t y\t Z\n");
-    printf("-------------------\n");
+    
     for (double x = xn; x <= xk + 0.0001; x += hx) {
           for (double y = yn; y <= yk + 0.0001; y += hy) {
             double z = calculateZ(x, y);
-            
+
             if (z > 0.2) {
                 sum += z;
-                count++;
-                printf("%.2f\t%.2f\t%.6f\n", x, y, z);
+
             }
         }
     }
-    
-    printf("\n");
-    printf("Количество значений Z > 0.2: %d\n", count);
     printf("Сумма значений Z > 0.2: %.6f\n", sum);
-    
+
     return 0;
 }
+
+//вывод:
+//Сумма значений Z > 0.2: 3.012972
+
