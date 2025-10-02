2d9adfb38999260593ca9cdb0dff433e1c65db1d
mariyanavsegda-alt <mariyanavsegda@gmail.com>
2025-10-02 20:39:44 +0300

    Update Lab1Variant28.c

--- Lab1Variant28.c
+++ Lab1Variant28.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <math.h>
 
+double calculateZ(double x, double y) {
+    if (x * y * y < 2) {
+        // Z = min(1 - sin(x² + y³), √(x²))
+        double term1 = 1 - sin(x * x + y * y * y);
+        double term2 = sqrt(x * x);
+        return (term1 < term2) ? term1 : term2;
+    } else {
+        // Z = √(x² * y + 8)
+        return sqrt(x * x * y + 8);
+    }
+}
+
+int main() {
+    double hx = 0.5;
+    double hy = 1.2;
+    double xn = 0.2;
+    double xk = 1.0;
+    double yn = 1.0;
+    double yk = 3.0;
+
+    double sum = 0.0;
+
+
+    for (double x = xn; x <= xk + 0.0001; x += hx) {
+          for (double y = yn; y <= yk + 0.0001; y += hy) {
+            double z = calculateZ(x, y);
+
+            if (z > 0.2) {
+                sum += z;
+
+            }
+        }
+    }
+    printf("Сумма значений Z > 0.2: %.6f\n", sum);
+
+    return 0;
+}
+//вывод
+//Сумма значений Z > 0.2: 3.012972
