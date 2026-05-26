8a745667626293bbbfc480c0166879d8518b91a0
mariyanavsegda-alt <mariyanavsegda@gmail.com>
2026-05-26 15:20:08 +0300

    Update main.cpp

--- laptopsCpp/main.cpp
+++ laptopsCpp/main.cpp
@@ -1,6 +1,9 @@
 #include "baza_noutov.h"
 #include <iostream>
+#include <limits>
+
 using namespace std;
+
 void show_menu() {
     cout << "\n1. Показать все\n"
          << "2. Добавить\n"
@@ -11,21 +14,36 @@ void show_menu() {
          << "7. Выход\n"
          << "Выбор: ";
 }
+
 int main(int argc, char* argv[]) {
+
     string filename = (argc > 1) ? argv[1] : "laptops.txt";
+
     LaptopDatabase db(filename);
+
     int choice;
+
     do {
         show_menu();
-        cin >> choice;
-        cin.ignore();
+
+        while (!(cin >> choice)) {
+            cout << "Введите число: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
         switch (choice) {
+
         case 1:
             db.show_all();
             break;
+
         case 2:
             db.add_laptop(input_laptop());
             break;
+
         case 3: {
             string brand;
             cout << "Бренд: ";
@@ -33,36 +51,55 @@ int main(int argc, char* argv[]) {
             db.find_by_brand(brand);
             break;
         }
+
         case 4: {
             int price;
             cout << "Макс. цена: ";
-            cin >> price;
-            cin.ignore();
+            while (!(cin >> price)) {
+                cout << "Введите число: ";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
             db.find_by_price(price);
             break;
         }
+
         case 5: {
             int id;
             cout << "ID: ";
-            cin >> id;
-            cin.ignore();
+            while (!(cin >> id)) {
+                cout << "Введите число: ";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
             db.update_laptop(id, input_laptop());
             break;
         }
+
         case 6: {
             int id;
             cout << "ID: ";
-            cin >> id;
-            cin.ignore();
+            while (!(cin >> id)) {
+                cout << "Введите число: ";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
             db.remove_laptop(id);
             break;
         }
+
         case 7:
-            cout << "Выход.\n";
+            cout << "Выход\n";
             break;
+
         default:
             cout << "Неверный ввод\n";
         }
+
     } while (choice != 7);
+
     return 0;
 }
